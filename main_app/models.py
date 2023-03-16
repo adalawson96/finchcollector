837483f4fbfeca165aa4ec39185a441b5e5f6e32
adalawson96 <adalawson96@gmail.com>
2023-03-16 15:08:51 +0100

    initial commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
-name = models.CharField(max_length=100)
-origin = models.CharField(max_length=100)
-description = models.TextField(max_length=250)
-age = models.IntegerField()
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
 
     #changing this instance method does not impact the database, therefore no makemigrations is necessaary
+    def __str__(self):
+        return f'{self.name} ({self.id})'
 
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -8,6 +10,17 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch 
+    })
+
+class FinchCreate(CreateView):
+   model = Finch
+   fields = '__all__'
